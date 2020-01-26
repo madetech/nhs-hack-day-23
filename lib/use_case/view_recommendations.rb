759f6e6a8f41020b431f740eff01f66ff0a02330
Tom Taylor <tommotaylor@gmail.com>
2020-01-26 10:11:46 +0000

    Add form to recommendations page

--- lib/use_case/view_recommendations.rb
+++ lib/use_case/view_recommendations.rb
@@ -2,8 +2,15 @@ module UseCase
   class ViewRecommendations
     def execute(*)
       { recommendations: [
-        { title: 'Walking Group',
-          description: '???' }
+        {
+          title: 'Walking Group',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud'
+        },
+        {
+          title: 'Tai Chi',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud'
+
+        }
       ] }
     end
   end
