f184032a22bc6ed6373b9d74c728974befba49b8
Craig J. Bass <craig@madetech.co.uk>
2020-01-26 13:14:51 +0000

    Quick options

--- lib/routes.rb
+++ lib/routes.rb
@@ -23,6 +23,16 @@ get '/seeds' do
   redirect '/'
 end
 
+get '/adam' do
+  @recommender.new_discharge_summary("Adam (21) has been seen by the Occupational Therapist following a spinal cord injury. He is a new wheelchair user. He is keen to return to paid employment and sport. Prior to his injury, he was a carer for his Mum.")
+  redirect '/'
+end
+
+get '/betty' do
+  @recommender.new_discharge_summary("Betty (85) was referred for Physiotherapy to improve her mobility after being in hospital due to a fall and broken hip (which was repaired). She is now able to walk short distances with a walking stick and is keen to progress her walking. She continues to be the main carer for her husband, who has moderate Alzheimer's.")
+  redirect '/'
+end
+
 get '/' do
   erb :discharge_summary_step, locals: { success: request['success'] == 'true' }
 end
