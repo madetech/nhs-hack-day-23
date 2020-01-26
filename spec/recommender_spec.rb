09392ee58655c1de3be3e4c5d2893d5c0f97c98a
Craig J. Bass <craig@madetech.co.uk>
2020-01-26 11:21:19 +0000

    tcr

--- spec/recommender_spec.rb
+++ spec/recommender_spec.rb
@@ -5,29 +5,33 @@ describe Recommender do
     recommender.new_discharge_summary(*args)
   end
 
+  def recommendations_summary
+    recommender.recommendations_summary
+  end
+
+  def recommendations
+    recommender.recommendations
+  end
+
   it 'can store the discharge summary' do
     new_discharge_summary('frail, and various other things')
-    discharge_summary = recommender.recommendations_summary[:discharge_summary]
+    discharge_summary = recommendations_summary[:discharge_summary]
     expect(discharge_summary).to eq('frail, and various other things')
   end
 
 
   it 'can show no recommendations for a blank discharge summary' do
-    recommendations = recommender.recommendations
     expect(recommendations).to eq([])
   end
 
   it 'can show recommendations when there is a discharge summary' do
-    recommender.new_discharge_summary('this is a discharge summary')
-    recommendations = recommender.recommendations
+    new_discharge_summary('this is a discharge summary')
     expect(recommendations[0][:title]).to eq('Tai Chi')
     expect(recommendations[1][:title]).to eq('Walking Group')
   end
 
   it 'can show the agreed recommendations at the end of the consultation' do
-    summary = recommender.recommendations_summary
-
-    recommendations = summary[:recommendations]
+    recommendations = recommendations_summary[:recommendations]
     expect(recommendations[0][:title]).to eq('Tai Chi')
     expect(recommendations[1][:title]).to eq('Walking Group')
   end
