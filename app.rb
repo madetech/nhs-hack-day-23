b0a8898556c21587137bcf90e85b57dc75cacb1b
Craig J. Bass <craig@madetech.com>
2020-01-26 20:09:33 +0000

    Remove these because the dyno keeps dying

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
-require 'rack-livereload'
-require 'sinatra/reloader'
+# require 'rack-livereload'
+# require 'sinatra/reloader'
 require 'raven'
 require 'sinatra/cookies'
 
 require_relative './loader'
 
-use Rack::LiveReload
+# use Rack::LiveReload
 use Raven::Rack
 
 require 'routes'
